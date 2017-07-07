1936c6590509c358a138098928761e34ee84a018
Ellen Liu <ellenliu@Ellens-MacBook-Pro.local>
2017-07-07 12:36:44 -0700

    Added token based authentication

--- Lesson_4/08 _Regal Tree Foods/starter_code/views.py
+++ Lesson_4/08 _Regal Tree Foods/starter_code/views.py
@@ -16,13 +16,26 @@ session = DBSession()
 app = Flask(__name__)
 
 
+# Verifies user's username or token with the password
+@auth.verify_password
+def verify_password(username_or_token, password):
+    #Try to see if it's a token first
+    user_id = User.verify_auth_token(username_or_token)
+    if user_id:
+        user = session.query(User).filter_by(id = user_id).one()
+    else:
+        user = session.query(User).filter_by(username = username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
 
-
-#ADD @auth.verify_password decorator here
-
-
-#add /token route here to get a token for a user with login credentials
-
+# Token for a user with login credentials
+@app.route('/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token()
+    return jsonify({'token': token.decode('ascii')})
 
 
 
